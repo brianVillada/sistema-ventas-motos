420b39126bdc03d292e7068d03568ec740a08909
brianVillada <39478670+brianVillada@users.noreply.github.com>
2026-03-14 16:53:08 -0500

    Add fields codigo and activo to Producto class

--- src/main/java/com/example/ventas/model/Producto.java
+++ src/main/java/com/example/ventas/model/Producto.java
@@ -8,14 +8,21 @@ public class Producto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
     private Long id;
 
     private String nombre;
 
     private double precio;
 
+    private String codigo;
+
+    private boolean activo;
+
     @ManyToOne
-    @JoinColumn(name = "categoria_id")
+    @JoinColumn(name = "id_categoria")
     private Categoria categoria;
 
+    @OneToOne(mappedBy = "producto")
+    private Inventario inventario;
 }
